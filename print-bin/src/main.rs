80d2aab01540377596e7a87132c284240db8de71
Malte Tammena <malte.tammena@pm.me>
2021-09-06 21:12:26 +0200

    Adjust test device

--- print-bin/src/main.rs
+++ print-bin/src/main.rs
@@ -1,6 +1,12 @@
 use escpos_lib::Printer;
+use serialport::{DataBits, Parity, StopBits, FlowControl};
 
 fn main() {
-    let port = serialport::new("/dev/serial0", 9600).open_native().unwrap();
+    let port = serialport::new("/dev/serial0", 9600).data_bits(DataBits::Eight)
+        .parity(Parity::None)
+        .stop_bits(StopBits::One)
+        .flow_control(FlowControl::None)
+        .open_native()
+        .unwrap();
     let printer = Printer::new(port);
 }
